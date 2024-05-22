76f419998b32eec067d3c6b8bbc10783f989eb1e
Mihir9702 <mkpatel9702@gmail.com>
2024-05-22 12:52:36 -0400

    remove secrets

--- src/utils/responseHandler.ts
+++ src/utils/responseHandler.ts
@@ -2,7 +2,7 @@ import { ResponseHandler } from './types'
 
 export function responseHandler(props: ResponseHandler) {
   if (props.response.error?.graphQLErrors) {
-    props.setError(props.response.error.graphQLErrors[0].message)
+    props.setError(props.response.error.graphQLErrors[0]?.message)
   } else {
     props.action === 'back' ? props.router.back() : props.router.push('/')
     location.reload()
